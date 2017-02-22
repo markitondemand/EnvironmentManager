968cc0f923d9d9bcde842fecf5aa488afdee6035
Michael Leber <michael.leber@markit.com>
2017-02-22 13:47:32 -0700

    Initial Commit

---
+++ MDEnvironmentManager/MDEnvironmentManager.h
@@ -0,0 +1,19 @@
+//
+//  MDEnvironmentManager.h
+//  MDEnvironmentManager
+//
+//  Created by Michael Leber on 2/22/17.
+//  Copyright © 2017 Markit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDEnvironmentManager.
+FOUNDATION_EXPORT double MDEnvironmentManagerVersionNumber;
+
+//! Project version string for MDEnvironmentManager.
+FOUNDATION_EXPORT const unsigned char MDEnvironmentManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDEnvironmentManager/PublicHeader.h>
+
+
